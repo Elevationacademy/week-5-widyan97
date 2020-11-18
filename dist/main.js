2a5b7adbc37f04eab56da994dfbed29032557b80
widyan97 <73026713+widyan97@users.noreply.github.com>
2020-11-18 10:09:41 +0200

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -6,11 +6,3 @@ const priceFurniture = function () {
     })
 }
 
-const buyFurniture = function () {
-    let input = $("#furniture-input").val()
-
-    $.get(`buy/${input}`, function (furnitureData) {
-        $("body").append(`Congratulations, you've just bought <div>${furnitureData.name} for ${furnitureData.price} .
-         There are ${furnitureData.inventory} left now in the store `)
-    })
-}
