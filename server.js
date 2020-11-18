f5143fa5a4c149a595d5b7d3c98bf1a5031f1ac9
widyan97 <73026713+widyan97@users.noreply.github.com>
2020-11-18 10:08:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-// data 
 
 const store = [
     { name: "table", inventory: 3, price: 800 },
@@ -11,9 +10,9 @@ const store = [
     { name: "picture frame", inventory: 31, price: 70 }
 ]
 
-//end of data 
+ 
 
-/// midleman 
+
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
@@ -29,28 +28,6 @@ app.get('/priceCheck/:name', function(request, response){
     response.send({"price":`null`})
 })
 
-app.get('/buy/:name', function(request, response){
-    let name = request.params.name
-    //let inventory
-  
-    for(let index of store ){
-        if(name == index.name){
-           if (index.inventory>0){
-            index.inventory--
-            response.send({"inventory":`${index.inventory}`,"name":`${index.name}`,"price":`${index.price}`})
-           }
-           else if (index.inventory <= 0){
-            response.send({"inventory":`0`})
-           }
-        }
-    }
-
-  
-    response.send({"inventory":`null`})
-})
-
-
-//port listner
 const port = 3001
 app.listen(port, function(){
     console.log(`Server is up and running smoothly ${port}`)
