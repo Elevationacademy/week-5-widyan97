8b152f9069abf8713383f8053083864cf5d9fde6
widyan97 <73026713+widyan97@users.noreply.github.com>
2020-11-18 11:07:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,22 +10,38 @@ const store = [
     { name: "picture frame", inventory: 31, price: 70 }
 ]
 
- 
-
-
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
 
 app.get('/priceCheck/:name', function(request, response){
     let name = request.params.name
-    for(let index of store ){
-        if(name == index.name){
-            response.send({"price":`${index.price}`})
+    for(let i in store ){
+        if(name == store[i].name){
+            response.send({"price":`${store[i].price}`})
         }
     }
 
     response.send({"price":`null`})
+})
+
+app.get('/buy/:name', function(request, response){
+    let name = request.params.name
+  
+    for(let i in store){
+        if(name == store[i].name){
+           if (store[i].inventory>0){
+            store[i].inventory--
+            response.send({"inventory":`${store[i].inventory}`,"name":`${store[i].name}`,"price":`${store[i].price}`})
+           }
+           else if (store[i].inventory <= 0){
+            response.send({"inventory":`0`})
+           }
+        }
+    }
+
+  
+    response.send({"inventory":`null`})
 })
 
 const port = 3000
